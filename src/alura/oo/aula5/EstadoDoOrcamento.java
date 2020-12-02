251a5614f875e740a59747d1528b579a047f0914
vinnom <vinnom.2d@gmail.com>
2020-12-02 00:31:19 -0300

    pequena refatoração na interface EstadoDoOrcamento

--- src/alura/oo/aula5/EstadoDoOrcamento.java
+++ src/alura/oo/aula5/EstadoDoOrcamento.java
@@ -3,7 +3,6 @@ package alura.oo.aula5;
 public interface EstadoDoOrcamento{
 
    void aplicaDescontoExtra(Orcamento orcamento);
-
    void aprova(Orcamento orcamento);
    void reprova(Orcamento orcamento);
    void finaliza(Orcamento orcamento);
